3cbfcb54e6661949cb24c8c87167502e415e1530
marco-tataranni <tataranni.marco@iisgalvanimi.edu.it>
2019-05-03 07:45:08 +0000

    f1

---
+++ calcolatrice/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import {  FormsModule, ReactiveFormsModule} from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, NgbModule,FormsModule, ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
